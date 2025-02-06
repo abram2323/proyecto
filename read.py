d47ddf1e2cc3e0f86bcf825cea01060e13f73c28
Abraham <abrahammm2001@gmail.com>
2025-02-06 11:52:00 +0100

    Documentación código

--- read.py
+++ read.py
@@ -1,14 +1,14 @@
 '''
-Comenta aqui
+carga el fichero y enseña el contenido
 '''
 def read_and_sort_file(file_path):
 	with open(file_path, 'r', encoding='utf-8') as file:
 		names = file.readlines()
 	
-	# Comenta aqui
+	# Coge el fichero y guarda el contenido en la variable "names"
 	names = [name.strip() for name in names]
 	names.sort()
 	
-	# Comenta aqui
+	# Es un bucle que muestra por pantalla los nombres
 	for name in names:
 		print(name)
